6dd228e50ebd4ba510ed94102d6d29fb74590185
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-25 14:32:20 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,9 +1,6 @@
 module FancyDance
   module InstanceMethods              # you "include" these instance methods
     include Dance
-    def superjump
-      "I'm just one jumping instance"
-    end
   end
   module ClassMethods                 # you "extend" these class methods
     include MetaDancing
